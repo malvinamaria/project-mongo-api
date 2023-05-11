a9668c66407c506ffc0c585d0a7373e8386fc53d
malvinamaria <mmpolak@protonmail.com>
2023-05-11 10:04:54 +0200

    add

--- server.js
+++ server.js
@@ -13,8 +13,7 @@ dotenv.config();
 // import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
 
-// const mongoUrl =
-//   process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/project-mongo';
+// connecting to mongoDB database running on localhost
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-mongo';
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
@@ -22,7 +21,7 @@ mongoose.Promise = Promise;
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 7080;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
@@ -47,9 +46,17 @@ app.get('/', (req, res) => {
   // res.send(process.env.API_KEY);
   // getting all users from mongoDB database running on localhost
   User.find().then((users) => {
-    res.json(users);
+    // fetch all users from the database
+    res.json(users); // send this back to the browser
   });
   // res.send('Hello Technigo!');
+});
+
+// show UserChild data
+app.get('/userchild', (req, res) => {
+  UserChild.find().then((userchild) => {
+    res.json(userchild);
+  });
 });
 
 // schema for user data in mongoDB database
@@ -63,6 +70,18 @@ const userSchema = new Schema({
 
 // model for user data in mongoDB database
 const User = mongoose.model('User', userSchema);
+
+// add another endpoint that will take age from the User model
+const UserChild = mongoose.model('UserChild', userSchema);
+
+const UserChildSchema = new Schema({
+  favoriteFood: String,
+  favoriteColour: String,
+  // email: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: 'User',
+  // },
+});
 
 // endpoint for getting all users
 const songSchema = new Schema({
@@ -87,8 +106,9 @@ const Song = mongoose.model('Song', songSchema);
 
 // endpoint for getting all songs
 app.get('/songs/id/:id', async (req, res) => {
+  // async is used to make sure that the code is executed in the right order
   try {
-    const singleSong = await Song.findById(req.params.id);
+    const singleSong = await Song.findById(req.params.id); // find song by id in the database and store it in a variable  called singleSong
     if (singleSong) {
       res.status(200).json({
         success: true,
@@ -114,6 +134,8 @@ app.get('/songs/id/:id', async (req, res) => {
 });
 
 // first create new users and wrapping new User with deleteMany to prevent duplicates
+// if (process.env.RESET_DB) {
+//   console.log('Resetting database!');
 User.deleteMany().then(() => {
   new User({
     name: 'Kalle',
@@ -133,7 +155,25 @@ User.deleteMany().then(() => {
     alive: true,
     email: 'lovet@test.com',
   }).save();
+  new User({
+    name: 'Luba',
+    age: 0,
+    alive: true,
+    email: 'luba@test.com',
+  }).save();
+  // new UserChild({
+  //   favoriteFood: 'pizza',
+  //   favoriteColour: 'blue',
+  //   email: '5f9f1b3b1c9d440000a0b0b1',
+  // }).save();
+
+  // new InsideUser({
+  //   favoriteFood: 'pizza',
+  //   age: '5f9f1b3b1c9d440000a0b0b1',
+  // }).save();
 });
+// }
+
 // with findOne we can search for a specific user
 app.get('/:name', async (req, res) => {
   User.findOne({ name: req.params.name }).then((user) => {
