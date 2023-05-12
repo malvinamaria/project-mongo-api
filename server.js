7cfda7d28bf94ad8a33ba0125bd18f6fa8d0a67b
malvinamaria <mmpolak@protonmail.com>
2023-05-12 09:23:06 +0200

    thursday-session-with-Daniel

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -11,9 +8,8 @@ dotenv.config();
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import topMusicData from './data/top-music.json';
 
-// connecting to mongoDB database running on localhost
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-mongo';
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
@@ -21,69 +17,21 @@ mongoose.Promise = Promise;
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 7080;
+const port = process.env.PORT || 8080;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// set middlewear up to catch database errors
-// using Connection.prototype.readyState
-// 0 = disconnected, 1 = connected, 2 = connecting, 3 = disconnecting
-app.use((req, res, next) => {
-  if (mongoose.connection.readyState === 1) {
-    next();
-  } else {
-    res.status(503).json({ error: 'Service unavailable' });
-  }
-});
-
-// Start defining your routes here
-app.get('/', (req, res) => {
-  // how to deal with secret API key
-  // fetch('...', {headers: {Authorization: `my secret api key`}})
-  // res.send(process.env.API_KEY);
-  // getting all users from mongoDB database running on localhost
-  User.find().then((users) => {
-    // fetch all users from the database
-    res.json(users); // send this back to the browser
-  });
-  // res.send('Hello Technigo!');
-});
-
-// show UserChild data
-app.get('/userchild', (req, res) => {
-  UserChild.find().then((userchild) => {
-    res.json(userchild);
-  });
-});
-
-// schema for user data in mongoDB database
 const { Schema } = mongoose;
 const userSchema = new Schema({
   name: String,
   age: Number,
   alive: Boolean,
-  email: String,
 });
 
-// model for user data in mongoDB database
 const User = mongoose.model('User', userSchema);
-
-// add another endpoint that will take age from the User model
-const UserChild = mongoose.model('UserChild', userSchema);
-
-const UserChildSchema = new Schema({
-  favoriteFood: String,
-  favoriteColour: String,
-  // email: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: 'User',
-  // },
-});
-
-// endpoint for getting all users
 const songSchema = new Schema({
   id: Number,
   trackName: String,
@@ -101,14 +49,53 @@ const songSchema = new Schema({
   popularity: Number,
 });
 
-// model for song data in mongoDB database
 const Song = mongoose.model('Song', songSchema);
 
-// endpoint for getting all songs
-app.get('/songs/id/:id', async (req, res) => {
-  // async is used to make sure that the code is executed in the right order
+if (process.env.RESET_DB) {
+  const resetDatabase = async () => {
+    await Song.deleteMany();
+    topMusicData.forEach((singleSong) => {
+      const newSong = new Song(singleSong);
+      newSong.save();
+    });
+  };
+  resetDatabase();
+  // call a function while declaring it - extra curriculum
+}
+
+// Start defining your routes here
+app.get('/', (req, res) => {
+  res.send('Hello Technigo!');
+});
+
+app.get('/songs', async (req, res) => {
+  const { genre, danceability } = req.query;
+  const response = {
+    success: true,
+    body: {},
+  };
+  const genreRegex = new RegExp(genre);
+  const danceabilityQuery = { $gt: danceability ? danceability : 0 };
+
   try {
-    const singleSong = await Song.findById(req.params.id); // find song by id in the database and store it in a variable  called singleSong
+    const searchResultFromDB = await Song.find({
+      genre: genreRegex,
+      danceability: danceabilityQuery,
+    });
+    if (searchResultFromDB) {
+      response.body = searchResultFromDB;
+      res.status(200).json(response);
+    } else {
+      (response.success = false), res.status(500).json(response);
+    }
+  } catch (e) {
+    res.status(500).json(response);
+  }
+});
+
+app.get('/songs/id/:id', async (req, res) => {
+  try {
+    const singleSong = await Song.findById(req.params.id);
     if (singleSong) {
       res.status(200).json({
         success: true,
@@ -122,7 +109,6 @@ app.get('/songs/id/:id', async (req, res) => {
         },
       });
     }
-    // try is connected with catch, for bad requests
   } catch (e) {
     res.status(500).json({
       success: false,
@@ -133,57 +119,6 @@ app.get('/songs/id/:id', async (req, res) => {
   }
 });
 
-// first create new users and wrapping new User with deleteMany to prevent duplicates
-// if (process.env.RESET_DB) {
-//   console.log('Resetting database!');
-User.deleteMany().then(() => {
-  new User({
-    name: 'Kalle',
-    age: 30,
-    alive: true,
-    email: 'test@test.com',
-  }).save();
-  new User({
-    name: 'Savanah',
-    age: 1,
-    alive: true,
-    email: 'tepst@test.com',
-  }).save();
-  new User({
-    name: 'Chester',
-    age: 0,
-    alive: true,
-    email: 'lovet@test.com',
-  }).save();
-  new User({
-    name: 'Luba',
-    age: 0,
-    alive: true,
-    email: 'luba@test.com',
-  }).save();
-  // new UserChild({
-  //   favoriteFood: 'pizza',
-  //   favoriteColour: 'blue',
-  //   email: '5f9f1b3b1c9d440000a0b0b1',
-  // }).save();
-
-  // new InsideUser({
-  //   favoriteFood: 'pizza',
-  //   age: '5f9f1b3b1c9d440000a0b0b1',
-  // }).save();
-});
-// }
-
-// with findOne we can search for a specific user
-app.get('/:name', async (req, res) => {
-  User.findOne({ name: req.params.name }).then((user) => {
-    if (user) {
-      res.json(user);
-    } else {
-      res.status(404).json({ error: 'User not found' });
-    }
-  });
-});
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
